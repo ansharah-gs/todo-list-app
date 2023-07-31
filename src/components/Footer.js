aafd06207729477424f6b123c88e6704bd861ac1
ansharahmir <101030434+ansharahmir@users.noreply.github.com>
2023-07-31 10:33:28 -0600

    My first react app

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() {
+  return (
+    <div>
+      <h6>Copyright © 2023</h6>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+         const TodoList= () => {
+    
+        return(
+            <>
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+            </>
+        );
+     };
+export default TodoList;
